e8131cb5d90477e59eb0c196c3954ad6850d0f14
Lukas Lindgård Paley <lukaslp@stud.ntnu.no>
2025-10-21 12:12:58 +0200

    Lagt inn JS

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Henter referanser til HTML-elementene vi trenger
+const startButton = document.getElementById('start-button');
+const startScreen = document.getElementById('start-screen');
+const gameScreen = document.getElementById('game-screen');
+
+// Venter på at startknappen blir klikket
+startButton.addEventListener('click', () => {
+
+    // Skjuler startskjermen 
+    startScreen.classList.add('hidden');
+
+    // Viser spillskjermen 
+    gameScreen.classList.remove('hidden');
+});
