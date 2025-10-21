daf3b04e0a2563b27d4d2b092d5ddda8929872ba
Lukas Lindgård Paley <lukaslp@stud.ntnu.no>
2025-10-21 12:19:18 +0200

    Spill funksjoner legges inn

---
+++ js/game.js
@@ -0,0 +1,15 @@
+// Struktur for alle spillnivåer
+const gameData = },
+        trackB: { characters: ['Prosjektleder'] },
+        feedback: {
+            psychopath: "Effektivt! Prosjektlederen hadde sikkert fortjent det.",
+            sheep: "Du ofret flertallet for mindretallet, akkurat som forventet."
+        }
+    }
+    // Flere nivåer kan legges til her som nye objekter
+];
+
+// Hovedobjekt for spillet som vil inneholde all logikk
+const game = {
+    // Funksjoner legges til her etter hvert
+};
